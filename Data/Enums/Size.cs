bfb6fa1de2d3e2ba6b686f2c43285ea33465d52a
Nathan Bean <nhbean@ksu.edu>
2020-08-25 12:38:19 -0500

    Initial commit

---
+++ Data/Enums/Size.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Author: Zachery Brunner
+ * Class name: Size.cs
+ * Purpose: Class used to represent sizes through an enumeration
+ */
+
+namespace BleakwindBuffet.Data.Enums
+{
+    /// <summary>
+    /// Provides available sizes
+    /// </summary>
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
